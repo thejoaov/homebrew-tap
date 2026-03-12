db8e5c0524dd3c55a721780d1b65a18dc7c47c17
thejoaov <thejoaov@users.noreply.github.com>
2026-03-12 14:21:01 +0000

    chore(cask): tray-link 2.1.7

--- Casks/tray-link.rb
+++ Casks/tray-link.rb
@@ -1,8 +1,8 @@
 cask "tray-link" do
-  version "2.1.6"
-  sha256 "934c2ed82ea54c3fbd51d16b0d366622daaeae5ade87c50a0902dd7e09bb75f0"
+  version "2.1.7"
+  sha256 "a6667624ff378021818a3f7081496b5d7f6d23b03cb829fa974feea798d1e0b1"
 
-  url "https://github.com/thejoaov/tray-link/releases/download/v2.1.6/Tray-Link-macOS-universal.zip"
+  url "https://github.com/thejoaov/tray-link/releases/download/v2.1.7/Tray-Link-macOS-universal.zip"
   name "Tray Link"
   desc "Manage your projects from the system tray"
   homepage "https://github.com/thejoaov/tray-link"
