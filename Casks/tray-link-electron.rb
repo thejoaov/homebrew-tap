0df88a097a627e91cf1922221cb3765dacbfe8cc
thejoaov <thejoaov@users.noreply.github.com>
2026-06-16 22:00:19 +0000

    chore(cask): sync macOS casks 2.2.14

--- Casks/tray-link-electron.rb
+++ Casks/tray-link-electron.rb
@@ -1,8 +1,8 @@
 cask "tray-link-electron" do
-  version "2.2.13"
-  sha256 "93f7593231bae192db1a9586d4a7d89543e9b75087c222a3b56fce0ef9224f66"
+  version "2.2.14"
+  sha256 "107df0237b4886f7e008b2187a58502a9cd888a8ee43d07e6da7cf5ca3039f74"
 
-  url "https://github.com/thejoaov/tray-link/releases/download/v2.2.13/Tray.Link-darwin-arm64-2.2.13.zip"
+  url "https://github.com/thejoaov/tray-link/releases/download/v2.2.14/Tray.Link-darwin-arm64-2.2.14.zip"
   name "Tray Link"
   desc "Manage your projects from the system tray"
   homepage "https://github.com/thejoaov/tray-link"
--- Casks/tray-link.rb
+++ Casks/tray-link.rb
@@ -1,10 +1,10 @@
 cask "tray-link" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.2.13"
-  sha256 "cc7f14b22d2cb2b3438a1986dd747139303424a2a56198c3cbcd224acfcb4496"
+  version "2.2.14"
+  sha256 "7021b58fa4fa602cb1dadb722cbd67c046bbb01b04ed24b33b71a4ec7afdf93d"
 
-  url "https://github.com/thejoaov/tray-link/releases/download/v2.2.13/Tray-Link-macOS-universal.zip"
+  url "https://github.com/thejoaov/tray-link/releases/download/v2.2.14/Tray-Link-macOS-universal.zip"
   name "Tray Link"
   desc "Manage your projects from the system tray"
   homepage "https://github.com/thejoaov/tray-link"
